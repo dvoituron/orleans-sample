a22d6b974472ba4f12df44d0fc52d424fd639348
Denis Voituron <dvoituron@outlook.com>
2024-10-04 12:25:37 +0200

    Add GenerateSerializer attribute

--- src/UrlShortener/Models/UrlDetails.cs
+++ src/UrlShortener/Models/UrlDetails.cs
@@ -1,7 +1,11 @@
-﻿public record class UrlDetails
+﻿[GenerateSerializer()]
+[Alias("UrlDetails")]
+public record class UrlDetails
 {
+    [Id(0)]
     public required string FullUrl { get; set; }
-    
+
+    [Id(1)]
     public required string ShortenedRouteSegment { get; set; }
 
     public static UrlDetails Empty => new UrlDetails
